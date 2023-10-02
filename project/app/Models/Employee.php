8a0db182076ef72c7562b3e4cd642e7261f9081f
Daniel Valenzuela <danielvalenzuela199825@gmail.com>
2023-10-02 09:43:58 -0300

    First commit

---
+++ project/app/Models/Employee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "first_name",
+        "last_name",
+        "address",
+        "zip_code",
+        "date_of_birth",
+        "date_hired",
+        "country_id",
+        "state_id",
+        "city_id",
+    ];
+}
